c7bc63eecf928439359556e4cbf143453d091486
Jhon Mosk <jhon-mosk@yandex.ru>
2023-05-18 21:55:45 +0300

    обернул приложение в StyledEngineProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./css/normalize.css";
 import "./css/index.css";
 import App from "./components/App";
+import { StyledEngineProvider } from "@mui/material";
 
 //TODO switch to dark mode
 // https://mui.com/material-ui/customization/theming/#themeprovider
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <StyledEngineProvider injectFirst>
+        <App />
+    </StyledEngineProvider>
+);
